ae1e3b95ca1f5e5dba3048894b4fc4ff0b62c3fb
Derek Kennard <dkennard@homeinfusion.com>
2021-06-10 12:09:51 -0400

    Add project files.

---
+++ SystemLock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace SystemLock
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+
+        static void Main(string[] args)
+        {
+            LockWorkStation();
+        }
+    }
+}
